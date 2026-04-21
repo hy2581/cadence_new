e0b8b6b9a639982eb20a84301a0d738d29e350eb
hy258 <3627923058@qq.com>
2026-04-22 00:29:10 +0800

    cleanup: remove outdated root-level dev files, debug TBs, sensitive license bypass tools, and redundant docs

--- tb/agents/axi4_lite_agent/axi4_lite_sequencer.sv
+++
@@ -1,8 +0,0 @@
-// AXI4-Lite Sequencer
-class axi4_lite_sequencer extends uvm_sequencer #(axi4_lite_txn);
-    `uvm_component_utils(axi4_lite_sequencer)
-
-    function new(string name, uvm_component parent);
-        super.new(name, parent);
-    endfunction
-endclass
